b8f67df637e8e5707ccfffc2de004f583a9c6801
Megha2030 <meghadevaraj30@gmail.com>
2025-01-14 23:13:25 +0530

    11 Java Class program

---
+++ src/test/java/ex_02_java_basics/Lab012_printf.java
@@ -0,0 +1,10 @@
+package ex_02_java_basics;
+
+public class Lab012_printf {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+        System.out.print(a);
+        System.out.printf("value of a is %d",a);
+    }
+}
